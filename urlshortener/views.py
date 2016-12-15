727b8436ef14094a43662b086476d7ff99774912
natalya-erina <natalya.erina.96@gmail.com>
2016-12-15 13:57:42 +0300

    Check if generated url already exists in db

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -12,14 +12,25 @@ import random
 
 from urlshortener.models import URL, Image
 
+def generate_short_url(request):
+    short_url = request.META['HTTP_HOST'] + '/short_url/'
+    for _ in range(0, 6):
+        ch = random.choice(string.ascii_letters + string.digits)
+        short_url += str(ch)
+    return short_url
+
+def contains(short_url):
+    for u in URL.objects.all():
+        if u.short_url.__eq__(short_url):
+            return True
+    return False
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        short_url = '127.0.0.1:8000/short_url/'
-        for _ in range(0, 6):
-            ch = random.choice(string.ascii_letters + string.digits)
-            short_url += str(ch)
+        short_url = generate_short_url(request)
+        while contains(short_url):
+            short_url = generate_short_url(request)
         URL.objects.create(source_url=request.POST.get("long_url"), created_at=now(), short_url=short_url)
         return render_to_response('urlshortener/url.html', {'short_url':short_url})
     else:
@@ -33,13 +44,13 @@ def redirect_and_show_ad(request):
     for i in range(1,5):
         img = Image.objects.create(image_url='/static/'+ str(i) + '.jpg')
         images.append(img.image_url)
-    path = '127.0.0.1:8000' + request.get_full_path()
+    path = request.META['HTTP_HOST'] + request.get_full_path()
     original_url = ''
     for u in URL.objects.all():
         if u.short_url.__eq__(path):
             original_url = u.source_url
             break
-    response = render_to_response('urlshortener/show_ad.html', {'orig_url': original_url, 'images': images})
+    response = render_to_response('urlshortener/show_ad.html', {'images': images})
     response.set_cookie('orig_url', original_url)
     response.set_cookie('time', 15)
     return response
